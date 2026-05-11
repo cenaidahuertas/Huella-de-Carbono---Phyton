d47fe15c727b0d361eaa405d4d5b59f0d02472cd
Cenaida Huertas Rojas <chuertas21@unisalle.edu.co>
2026-05-10 20:17:06 -0500

    commit de cambios

--- Huellacarbonobase.py
+++ Huellacarbonobase.py
@@ -2,19 +2,9 @@
 from abc import ABC, abstractmethod
 
 class HuellaCarbonoBase(ABC):
-    """
-    Clase base que define el contrato para calcular
-    la huella de carbono de cualquier entidad.
-
-    Todas las clases que hereden de esta deben
-    implementar los metodos calcular_huella() y get_info().
-    """
 
     @abstractmethod
     def calcular_huella(self):
-        """
-        Calcula la huella de carbono anual en kg de CO2.
-        """
         raise NotImplementedError(
         )
 
@@ -24,12 +14,6 @@ class HuellaCarbonoBase(ABC):
         )
 
     def mostrar_resultado(self):
-        """
-        REUTILIZACION DE CODIGO:
-        Este metodo es heredado por TODAS las subclases.
-        Ninguna necesita volver a escribirlo.
-        Llama a calcular_huella() que cada una define diferente.
-        """
         print("-" * 50)
         print(self.get_info())
         print("Huella de carbono: {:.2f} kg CO2/año".format(
